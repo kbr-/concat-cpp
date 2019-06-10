a80a4fd7f06b0991c955bd9deff42f032afdbac3
Kamil Braun <kb346840@students.mimuw.edu.pl>
2019-06-11 00:50:18 +0200

    clean

--- app/Main.hs
+++ app/Main.hs
@@ -9,11 +9,8 @@
 
 module Main where
 
-import ConCat.Misc
 import ConCat.AltCat (toCcc, inl)
 import ConCat.Rebox ()
-import ConCat.Rep
-import GHC.Generics (U1(..),Par1(..),(:*:)(..),(:.:)(..))
 
 import Category
 
@@ -32,36 +29,8 @@ fun x = h (fst x) (snd x) + (snd x)
 -- f :: Int -> Int -> Int
 -- f x y = x + y
 
--- f :: Int -> Either Int Int
--- f x = inl x
-
--- g :: [Int] -> Int
--- g [] = 0
--- g (c:cs) = c + g cs
--- {-# NOINLINE g #-}
-
--- f :: Int -> Int
--- f x = g [1,2,3]
--- {-# NOINLINE f #-}
-
--- f :: Int -> List
--- f x = Nil
-
 f :: Int -> Either Int Bool
 f x = inl x
-
--- f :: Int -> E
--- f x = L
-
--- f :: Int -> X
--- f x = X
-
--- f :: Int -> (Int, Int, Int)
--- f x = (x, x, x)
-
---f :: Int -> List
---f x | x > 0 = Cons x (f (x - 1))
---f _ = Nil
 
 main :: IO ()
 main = putStrLn $ runKat 21 (toCcc f)
